7d2bad7def9db53523ab2cf33435846a5abc6a7d
Nathan <nquinn@medonesystems.com>
2020-08-15 21:57:32 -0400

    sdv

--- src/restaurant/models/location.enitity.ts
+++ src/restaurant/models/location.enitity.ts
@@ -5,7 +5,10 @@ import {
   BaseEntity,
   CreateDateColumn,
   UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
 } from 'typeorm';
+import axios from 'axios';
 
 @Entity('location')
 export class Location extends BaseEntity {
@@ -24,6 +27,25 @@ export class Location extends BaseEntity {
   @Column({ nullable: true })
   phone: string;
 
+  @Column({ nullable: true })
+  lat: string;
+
+  @Column({ nullable: true })
+  lon: string;
+
   @CreateDateColumn() public createdAt: Date;
   @UpdateDateColumn() public updatedAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async beforeCreate() {
+    const address = this.address1 + ' ' + this.address2;
+    const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAvHC8FkiK6As9_tmLBrWz3NbTtJoQO6Uk`;
+    const { data } = await axios.get(url);
+
+    const obj = data?.results[0]?.geometry;
+
+    this.lat = obj?.location?.lat;
+    this.lon = obj?.location?.lng;
+  }
 }
