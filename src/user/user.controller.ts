b8165e388534628e481837f673fd1d4d617b43b7
Nathan <nquinn@medonesystems.com>
2020-08-29 11:54:28 -0400

    wef

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Res, Get } from '@nestjs/common';
+import { Controller, Post, Res, Get, Body, Req } from '@nestjs/common';
 import { Crud } from '@nestjsx/crud';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
+import Stripe from 'stripe';
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/account/apikeys
-const stripe = require('stripe')('sk_test_His9L7RGJvdVRuuPOkCeuand');
+// const stripe = require('stripe')('sk_test_His9L7RGJvdVRuuPOkCeuand');
+const stripe = new Stripe('sk_test_His9L7RGJvdVRuuPOkCeuand', {
+  apiVersion: '2020-08-27',
+});
 @Crud({
   model: {
     type: User,
@@ -15,29 +19,14 @@ export class UserController {
   constructor(public service: UserService) {}
 
   @Post('checkout')
-  async checkout(@Res() res) {
-    console.log('hit end point');
-
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000,
-          },
-          quantity: 1,
-        },
-      ],
-      mode: 'payment',
-      success_url:
-        'https://elevated-column-284822.ue.r.appspot.com/user/success',
-      cancel_url: 'https://elevated-column-284822.ue.r.appspot.com/user/cancel',
+  async checkout(@Body() { token, total }, @Res() res) {
+    await stripe.charges.create({
+      amount: total * 100,
+      currency: 'usd',
+      description: 'cool',
+      source: token,
     });
-    res.json({ id: session.id });
+    res.send({ success: true });
   }
   @Get('success')
   success() {
